14649ecea128292bad9dcd62afcb3e07d529d79e
Good elder brother <Goodelderbrother@xx.com>
2021-08-13 16:49:34 +0800

    更改主页

--- htmlcss/慕课网项目/js/backtotop.js
+++ htmlcss/慕课网项目/js/backtotop.js
@@ -1,6 +1,5 @@
-var backtotop = document.getElementById('backtotop');
-backtotop.onclick = function(){
+var backtotop = document.querySelector(".backtotop");
+backtotop.onclick = () => {
     document.documentElement.scrollTop = 0;
     document.body.scrollTop = 0;
 }
- 
